ebf30beaf6f538de95d7de26a1752886c083466d
tyctw <zhangwe886@gmail.com>
2026-05-16 23:15:50 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -708,8 +708,8 @@ export default function App() {
                       className="flex-1 sm:flex-none px-3 py-2 bg-white rounded-xl border-2 border-slate-200 text-sm font-bold focus:outline-none focus:border-slate-900"
                     >
                       <option value="all">公私立不拘</option>
-                      <option value="公立">公立</option>
-                      <option value="私立">私立</option>
+                      <option value="公立">public</option>
+                      <option value="私立">private</option>
                     </select>
                     <select 
                       value={resultFilterType} 
@@ -717,7 +717,7 @@ export default function App() {
                       className="flex-1 sm:flex-none px-3 py-2 bg-white rounded-xl border-2 border-slate-200 text-sm font-bold focus:outline-none focus:border-slate-900"
                     >
                       <option value="all">科別不拘</option>
-                      <option value="普通科">public</option>
+                      <option value="普通科">普通科</option>
                       <option value="職業類科">職業類科</option>
                     </select>
                   </div>
