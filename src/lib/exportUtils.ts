eefab3c521ce1d8761a12b80308b4a0abb7e03ae
ymhs0208 <ymhs0208@gmail.com>
2026-08-08 16:07:33 +0800

    Add files via upload

--- src/lib/exportUtils.ts
+++ src/lib/exportUtils.ts
@@ -607,8 +607,8 @@ export const printResults = (data: any, regionName: string) => {
         .header::after {
           position: absolute;
           bottom: 0;
-          left: 12mm;
-          width: 49mm;
+          left: 0;
+          width: 100%;
           height: 4px;
           border-radius: 99px;
           background: linear-gradient(90deg, #38bdf8, #a78bfa);
